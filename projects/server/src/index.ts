309ff46ba0f749822b210774f3aa8f43b95881a0
RobinHoodie <robin@oreon.io>
2020-09-20 11:50:12 +0200

    Load environment variables in with dotenv

--- projects/server/src/index.ts
+++ projects/server/src/index.ts
@@ -1,8 +1,18 @@
 import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+import * as path from "path";
+import env from "dotenv";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-export const helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+env.config({ path: path.resolve(__dirname, "../.env.local") });
+
+admin.initializeApp({
+  storageBucket: "oreon-invoices.appspot.com",
+  credential: admin.credential.applicationDefault()
+});
+
+export const helloWorld = functions.https.onRequest(async (request, response) => {
+
+  const bucket = admin.storage().bucket();
+  const files = await bucket.getFiles();
+  response.send("Hello from Firebase!");
 });
